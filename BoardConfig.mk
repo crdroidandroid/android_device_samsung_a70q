a0d47fa814ab051c06d7cb09d0c62c27a8be8469
Haky86 <hakamelassouad@gmail.com>
2023-11-15 21:29:31 +0100

    a70q: Initial commit
    Change-Id: I34fab6c17323ac07b8f7c30bc7d93c6d0549756e

---
+++ BoardConfig.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/samsung/sm6150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/a70q
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := a70q
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Board
+TARGET_BOARD_NAME := SRPRL06C005
+
+# GPU
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
+
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 1
+TARGET_KERNEL_CONFIG := a70q_defconfig
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+# Security Patch Level
+VENDOR_SECURITY_PATCH := 2023-02-01
+
+# Get non-open-source specific aspects
+include vendor/samsung/a70q/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a70q
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+
+# Display
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit Common Device Tree
+$(call inherit-product, device/samsung/sm6150-common/sm6150.mk)
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/a70q/a70q-vendor.mk)
